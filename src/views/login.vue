ae353a5d5065dd47326de974ef8691cd8d3e4abc
leimeng <1732010407ray@sina.com>
2022-04-15 11:37:45 +0800

    22

---
+++ src/views/login.vue
@@ -0,0 +1,92 @@
+<template>
+    <div class="homepage-hero-module">
+    <div class="video-container">
+      <video :style="fixStyle" muted autoplay loop class="fillWidth" src="../assets/loginBG.mp4">
+        浏览器不支持 video 标签，建议升级浏览器。
+      </video>
+    </div>
+</div>
+    <!-- <el-container>
+      <el-header>Header</el-header>
+      <el-main>Main</el-main>
+      <el-footer>Footer</el-footer>
+    </el-container> -->
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data () {
+    return {
+      fixStyle: ''
+    }
+  },
+  mounted () {
+    window.onresize = () => {
+      const windowWidth = document.body.clientWidth
+      const windowHeight = document.body.clientHeight
+      const windowAspectRatio = windowHeight / windowWidth
+      let videoWidth
+      let videoHeight
+      if (windowAspectRatio < 0.5625) {
+        videoWidth = windowWidth
+        videoHeight = videoWidth * 0.5625
+        this.fixStyle = {
+          height: windowWidth * 0.5625 + 'px',
+          width: windowWidth + 'px',
+          'margin-bottom': (windowHeight - videoHeight) / 2 + 'px',
+          'margin-left': 'initial'
+        }
+      } else {
+        videoHeight = windowHeight
+        videoWidth = videoHeight / 0.5625
+        this.fixStyle = {
+          height: windowHeight + 'px',
+          width: windowHeight / 0.5625 + 'px',
+          'margin-left': (windowWidth - videoWidth) / 2 + 'px',
+          'margin-bottom': 'initial'
+        }
+      }
+    }
+    window.onresize()
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style>
+.el-header, .el-footer {
+  background-color: #B3C0D1;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
+.el-main {
+  background-color: #E9EEF3;
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+}
+.homepage-hero-module,
+  .video-container {
+    position: relative;
+    height: 100vh;
+    overflow: hidden;
+  }
+
+  .video-container .poster img,
+  .video-container video {
+    z-index: 0;
+    width: 100%;
+    height: 100%;
+    /* position: absolute; */
+  }
+
+  .video-container .filter {
+    z-index: 1;
+    position: absolute;
+    background: rgba(0, 0, 0, 0.4);
+  }
+</style>
